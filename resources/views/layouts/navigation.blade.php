8ae107fb4712cb8a0d38232f17dbd9b0918d87f4
rizal57 <fahrurrzl5798@gmail.com>
2022-11-09 10:54:06 +0700

    [IMP] Improve navbar

--- resources/views/layouts/navigation.blade.php
+++ resources/views/layouts/navigation.blade.php
@@ -1,28 +1,79 @@
-<nav x-data="{ open: false }" class="bg-white border-b border-gray-100">
+{{-- <header class="bg-white max-w-7xl mx-auto px-4 sm:px-6 lg:px-8 py-4">
+    <div></div>
+    <div class="flex items-center">
+        <!-- Logo -->
+        <div class="shrink-0 flex items-center">
+            <a href="{{ route('dashboard') }}">
+                <x-application-logo class="block h-10 w-auto fill-current text-gray-600" />
+            </a>
+        </div>
+    </div>
+</header> --}}
+<nav x-data="{ open: false }" class="bg-slate-50 border-b border-gray-100">
     <!-- Primary Navigation Menu -->
     <div class="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
-        <div class="flex justify-between h-16">
-            <div class="flex">
+        <div class="flex items-center gap-4 justify-between h-16">
+            <div class="flex items-center">
                 <!-- Logo -->
                 <div class="shrink-0 flex items-center">
                     <a href="{{ route('dashboard') }}">
                         <x-application-logo class="block h-10 w-auto fill-current text-gray-600" />
                     </a>
                 </div>
-
                 <!-- Navigation Links -->
                 <div class="hidden space-x-8 sm:-my-px sm:ml-10 sm:flex">
                     <x-nav-link :href="route('dashboard')" :active="request()->routeIs('dashboard')">
                         {{ __('Dashboard') }}
                     </x-nav-link>
                 </div>
+
+
             </div>
 
+            {{-- searh start --}}
+            <div class="w-full">
+                <div class="form-control">
+                    <input type="text" placeholder="Search products..." class="py-1 px-2 rounded-lg bg-white border-slate-300 placeholder:text-slate-300" />
+                </div>
+            </div>
+            {{-- searh end --}}
             <!-- Settings Dropdown -->
-            <div class="hidden sm:flex sm:items-center sm:ml-6">
+            <div class="hidden sm:flex sm:items-center sm:gap-2 sm:ml-6 lg:gap-4">
+                {{-- cart start --}}
                 <x-dropdown align="right" width="48">
                     <x-slot name="trigger">
-                        <button class="flex items-center text-sm font-medium text-gray-500 hover:text-gray-700 hover:border-gray-300 focus:outline-none focus:text-gray-700 focus:border-gray-300 transition duration-150 ease-in-out">
+                        <label tabindex="0" class="btn btn-ghost btn-circle">
+                            <div class="indicator">
+                                <svg xmlns="http://www.w3.org/2000/svg" class="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M3 3h2l.4 2M7 13h10l4-8H5.4M7 13L5.4 5M7 13l-2.293 2.293c-.63.63-.184 1.707.707 1.707H17m0 0a2 2 0 100 4 2 2 0 000-4zm-8 2a2 2 0 11-4 0 2 2 0 014 0z" /></svg>
+                                <span class="badge badge-sm indicator-item">8</span>
+                            </div>
+                        </label>
+                        <x-slot name="content">
+                            <div tabindex="0" class="mt-3 card card-compact dropdown-content w-52 bg-base-100 shadow">
+                                <div class="card-body">
+                                    <span class="font-bold text-lg">8 Items</span>
+                                    <span class="text-info">Subtotal: $999</span>
+                                    <div class="card-actions">
+                                        <button class="btn btn-primary btn-block">View cart</button>
+                                    </div>
+                                </div>
+                            </div>
+                        </x-slot>
+                    </x-slot>
+                </x-dropdown>
+                {{-- card end --}}
+
+                {{-- profile image start --}}
+                <x-dropdown align="right" width="48">
+                    <x-slot name="trigger">
+                        <div class="dropdown dropdown-end">
+                            <label tabindex="0" class="btn btn-ghost btn-circle avatar">
+                                <div class="w-10 rounded-full">
+                                    <img src="https://placeimg.com/80/80/people" />
+                                </div>
+                            </label>
+                        </div>
+                        {{-- <button class="flex items-center text-sm font-medium text-gray-500 hover:text-gray-700 hover:border-gray-300 focus:outline-none focus:text-gray-700 focus:border-gray-300 transition duration-150 ease-in-out">
                             <div>{{ Auth::user()->name }}</div>
 
                             <div class="ml-1">
@@ -30,7 +81,7 @@
                                     <path fill-rule="evenodd" d="M5.293 7.293a1 1 0 011.414 0L10 10.586l3.293-3.293a1 1 0 111.414 1.414l-4 4a1 1 0 01-1.414 0l-4-4a1 1 0 010-1.414z" clip-rule="evenodd" />
                                 </svg>
                             </div>
-                        </button>
+                        </button> --}}
                     </x-slot>
 
                     <x-slot name="content">
@@ -46,10 +97,29 @@
                         </form>
                     </x-slot>
                 </x-dropdown>
+                {{-- profile imgae end --}}
             </div>
 
             <!-- Hamburger -->
             <div class="-mr-2 flex items-center sm:hidden">
+                <div class="flex-none">
+                    <div class="dropdown dropdown-end">
+                        <label tabindex="0" class="btn btn-ghost btn-circle">
+                            <div class="indicator">
+                                <svg xmlns="http://www.w3.org/2000/svg" class="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M3 3h2l.4 2M7 13h10l4-8H5.4M7 13L5.4 5M7 13l-2.293 2.293c-.63.63-.184 1.707.707 1.707H17m0 0a2 2 0 100 4 2 2 0 000-4zm-8 2a2 2 0 11-4 0 2 2 0 014 0z" /></svg>
+                                <span class="badge badge-sm indicator-item">8</span>
+                            </div>
+                        </label>
+                        <div tabindex="0" class="mt-3 card card-compact dropdown-content w-52 bg-base-100 shadow">
+                        <div class="card-body">
+                            <span class="font-bold text-lg">8 Items</span>
+                            <span class="text-info">Subtotal: $999</span>
+                            <div class="card-actions">
+                            <button class="btn btn-primary btn-block">View cart</button>
+                            </div>
+                        </div>
+                    </div>
+                </div>
                 <button @click="open = ! open" class="inline-flex items-center justify-center p-2 rounded-md text-gray-400 hover:text-gray-500 hover:bg-gray-100 focus:outline-none focus:bg-gray-100 focus:text-gray-500 transition duration-150 ease-in-out">
                     <svg class="h-6 w-6" stroke="currentColor" fill="none" viewBox="0 0 24 24">
                         <path :class="{'hidden': open, 'inline-flex': ! open }" class="inline-flex" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M4 6h16M4 12h16M4 18h16" />
@@ -58,6 +128,12 @@
                 </button>
             </div>
         </div>
+    </div>
+    <div class="flex items-center gap-4 w-full text-slate-500 pb-2">
+        <div>kategori</div>
+        <div>kategori</div>
+        <div>kategori</div>
+        <div>kategori</div>
     </div>
 
     <!-- Responsive Navigation Menu -->

c5a635478e44c9a6b2387bcad08a14ac01d87399
rizal57 <fahrurrzl5798@gmail.com>
2022-11-16 21:19:17 +0700

    [ADD] Add user settings

---
+++ resources/views/frontend/users/index.blade.php
@@ -0,0 +1,3 @@
+<x-app-layout>
+    <livewire:user-settings />
+</x-app-layout>
--- routes/web.php
+++ routes/web.php
@@ -2,23 +2,13 @@
 
 use App\Http\Controllers\Checkout;
 use App\Http\Controllers\HomeController;
-use App\Http\Controllers\ProductDetail as ControllersProductDetail;
-use App\Http\Controllers\ProductsController;
+use App\Http\Controllers\UserController;
 use App\Http\Livewire\Cart;
-use App\Http\Livewire\Home;
 use App\Http\Livewire\ProductDetail;
+use App\Http\Livewire\Profile;
+use App\Http\Livewire\UserSettings;
 use Illuminate\Support\Facades\Route;
 
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| contains the "web" middleware group. Now create something great!
-|
-*/
 
 Route::get('/', HomeController::class)->name('home');
 Route::get('product-detail/{slug}', ProductDetail::class)->name('product.detail');
@@ -27,6 +17,7 @@ Route::get('product-detail/{slug}', ProductDetail::class)->name('product.detail'
 Route::middleware('auth')->group(function() {
     Route::get('cart', Cart::class)->name('cart');
     Route::get('checkout', Checkout::class)->name('checkout');
+    Route::get('user/settings', [UserController::class, 'index'])->name('user.index');
 });
 
 require __DIR__.'/auth.php';
